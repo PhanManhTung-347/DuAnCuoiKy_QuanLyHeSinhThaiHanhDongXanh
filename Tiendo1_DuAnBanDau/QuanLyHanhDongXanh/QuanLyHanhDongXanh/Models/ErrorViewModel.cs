ee54488c8142bba52a430f82444fd915ac870855
PhanManhTung-347 <phanmanhtung24112006@gmail.com>
2026-05-10 13:55:40 +0700

    PhanManhTung_Taoduanbandau

---
+++ Tiendo1_DuAnBanDau/QuanLyHanhDongXanh/QuanLyHanhDongXanh/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QuanLyHanhDongXanh.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
